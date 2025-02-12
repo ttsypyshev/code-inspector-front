e9a1965af190915398858eb021613ef548628403
Timofei Tsypyshev <ttsypyshev01@gmail.com>
2025-02-12 10:14:14 +0300

    clear project
    del gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -12,12 +12,3 @@ createRoot(document.getElementById('root')!).render(
     </Provider>
   </StrictMode>,
 );
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(() => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
